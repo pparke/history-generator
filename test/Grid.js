d8b64b412d2ffb5125c1791adf3160621979b7d6
Philip Parke <philipparke@gmail.com>
2016-03-11 21:28:06 -0400

    evaporation method working

--- test/Grid.js
+++ test/Grid.js
@@ -1,29 +1,38 @@
 'use strict';
-let chai = require('chai');
-let assert = chai.assert;
-let should = chai.should();
-let Grid = require('../lib/world/Grid');
+let chai      = require('chai');
+let assert    = chai.assert;
+let should    = chai.should();
+let Grid      = require('../lib/world/Grid');
+let helpers   = require('../lib/math/helpers');
 
 describe('RandomDataGenerator', function() {
-  let grid = new Grid(20, 20, 2, 3);
+  let width = 2;
+  let height = 18;
+  let grid = new Grid(width, height, 2, 3);
 
   describe('#constructor', function () {
     it('ensure that the grid was properly initialized', function () {
-      assert.equal(grid.data.length, 20);
-      assert.equal(grid.data[0].length, 20);
 
-      let firstElem = grid.data[0][0];
-      firstElem.should.have.property('x');
-      firstElem.should.have.property('y');
-      firstElem.should.have.property('height');
-      console.log('first elem', firstElem);
+      assert.equal(grid.heightmap.length, width*height);
+      assert.equal(grid.heatmap.length, width*height);
+      assert.equal(grid.pressuremap.length, width*height);
+      assert.equal(grid.densitymap.length, width*height);
     });
   });
 
-  describe('#applyNoise', function () {
-    it('should apply the noise function to the data in the grid', function () {
-      let firstElem = grid.data[0][0];
-      console.log('first elem', firstElem);
+  describe('#generate', function () {
+    it('should generate new grid data', function () {
+      grid.generate();
+
+      for (let i = 0; i < grid.heatmap.length; i++) {
+        let temp = helpers.denormalize(grid.heatmap[i], -50, 50);
+        console.log(temp)
+      }
+      
+      //console.log(grid.heightmap.slice(0, 20));
+      //console.log(grid.heatmap);
+      //console.log(grid.pressuremap);
+      //console.log(grid.densitymap);
     });
   });
 });
